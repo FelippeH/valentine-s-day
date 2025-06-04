c82dd4049989643a3210d0e045cf37da4e425db4
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-06-04 15:28:45 -0300

    v_0

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function generateParticles() {
   }
 
   if (width < 600) {
-    const minGridSize = 9;
+    const minGridSize = 10;
     if (gridSize < minGridSize) {
       gridSize = minGridSize;
     }
